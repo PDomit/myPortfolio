b54859742617bcab90469d6b08fe82687757b9f1
PDomit <piotrdomiter@gmail.com>
2025-04-28 16:10:46 +0200

    Refreshing update

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // USTAWIANIE POCZĄTKU STRONY PO ODSWIEŻENIU
  window.onload = function(){
+  if (window.location.hash) {
+    history.replaceState(null, null, window.location.pathname);
+  }
   const box1 = document.getElementById('box1');
   const box2 = document.getElementById('box2');
   const box3 = document.getElementById('box3');
